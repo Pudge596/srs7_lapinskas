d2ff4754a3bcfc508ab20e53067d8577a6687ab6
mobil <dlapinskas04@gmail.com>
2023-12-20 15:13:59 +0700

    add

--- app/src/main/java/com/example/srs7/MainActivity4.java
+++ app/src/main/java/com/example/srs7/MainActivity4.java
@@ -2,20 +2,65 @@ package com.example.srs7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity implements View.OnClickListener {
-
+ private Button btn;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(this);
+        ValidationEmail validator = new ValidationEmail();
+        btn.setEnabled(false);
+        btn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#C9D4FB")));
+        EditText editText = findViewById(R.id.editTextTextEmailAddress);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                btn.setEnabled(false);
+                btn.getBackground();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                btn.setEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String inputText = editable.toString();
+                if(validator.validate(inputText))
+                {
+                    btn.setEnabled(true);
+                    btn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#1A6FEE")));
+                    btn.setTextColor(ColorStateList.valueOf(Color.WHITE));
+                }
+                else
+                {
+                    btn.setEnabled(false);
+                    btn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#C9D4FB")));
+                }
+            }
+        });
+
     }
 
     @Override
-    public void onClick(View view) {
-
+    public void onClick(View view)
+    {
+        Intent intent = new Intent(this,MainActivity5.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/srs7/MainActivity5.java
+++ app/src/main/java/com/example/srs7/MainActivity5.java
@@ -2,13 +2,78 @@ package com.example.srs7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Timer;
 
 public class MainActivity5 extends AppCompatActivity {
-
+ EditText number,number2,number3,number4;
+ int nober = 0;
+ TextView tvtimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        tvtimer = findViewById(R.id.textView19);
+        number = findViewById(R.id.editTextNumber2);
+        number2 = findViewById(R.id.editTextNumber4);
+        number3 = findViewById(R.id.editTextNumber5);
+        number4 = findViewById(R.id.editTextNumber6);
+        addAutoMoveTONExtEditText(number,number2,number);
+        addAutoMoveTONExtEditText(number2,number3,number);
+        addAutoMoveTONExtEditText(number3,number4,number2);
+        addAutoMoveTONExtEditText(number4,number4,number3);
+        Timer();
+
+    }
+
+    private void addAutoMoveTONExtEditText(final EditText current,final EditText next, final EditText Forerunner) {
+        current.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.length()==1){
+                    next.requestFocus();
+                    nober++;
+                    generatepassword(nober);
+
+                }
+                else{Forerunner.requestFocus();nober--;}
+                generatepassword(nober);
+            }
+        });
+    }
+    public void Timer(){
+        new CountDownTimer(60000,1000){
+            @SuppressLint("SetTextO18n")
+            public void onTick(long millisUntilFinished){
+                tvtimer.setText("Отправить код повторно можно будет через" +millisUntilFinished/1000+"секунд");
+            }
+            public void onFinish(){
+                tvtimer.setText("Можете запросить новый код");
+            }
+        }.start();
+    }
+    public void generatepassword(int nober){
+        if (nober == 4){
+            Intent intent = new Intent(MainActivity5.this, MainActivity6.class);
+            startActivity(intent);
+        }
     }
 }
